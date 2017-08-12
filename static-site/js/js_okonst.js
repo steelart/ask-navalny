78a09215a278839e09b3f08705034228db4435f0
Konst <sborus.com@gmail.com>
2017-08-12 23:30:00 +0300

    (Static) Question and answers page

---
+++ static-site/js/js_okonst.js
@@ -0,0 +1,11 @@
+// BX-SLIDER
+/*$(document).ready(function(){
+	$('.bxslider').bxSlider({
+	  //video: true,
+  	  //useCSS: false,
+	  minSlides: 3,
+	  maxSlides: 2,
+	  slideWidth: 560,
+	  slideMargin: 10
+	});
+});*/
